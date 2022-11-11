c6e02f028c87b1fe534fabf99f8c6442b41cea3a
azz <4zv4l@protonmail.com>
2022-11-11 19:07:31 +0100

    formating code

--- client/client.c
+++ client/client.c
@@ -7,45 +7,45 @@
 #define MAX 4048 // 4kb buffer
 
 void handle(int sockfd) {
-	char *input;
+    char *input;
     char buff[MAX];
-	ssize_t bytes;
+    ssize_t bytes;
     for (;;) {
-		// get user input
-		input = readline("$ ");
+        // get user input
+        input = readline("$ ");
 
-		// send cmd to socket
+        // send cmd to socket
         write(sockfd, input, strlen(input));
 
-		// if send exit then close
+        // if send exit then close
         if ((strncmp(input, "exit", 4)) == 0) {
-			printf("exiting...\n");
-			free(input);
+            printf("exiting...\n");
+            free(input);
             break;
         }
 
-		// if send STOP then close
+        // if send STOP then close
         if ((strncmp(input, "STOP", 4)) == 0) {
-			printf("shutting down the server...\n");
+            printf("shutting down the server...\n");
             free(input);
-			break;
+            break;
         }
 
-		// free the non needed input
-		free(input);
+        // free the non needed input
+        free(input);
 
-		// read cmd output from socket
-		bytes = read(sockfd, buff, sizeof(buff));
+        // read cmd output from socket
+        bytes = read(sockfd, buff, sizeof(buff));
         printf("%.*s", (int)bytes, buff);
     }
 }
-   
+
 int main(int argc, char **argv) {
-	// check arguments
-	if (argc != 3) {
-		printf("Usage: %s [ip] [port]\n", argv[0]);
-		return 1;
-	}
+    // check arguments
+    if (argc != 3) {
+        printf("Usage: %s [ip] [port]\n", argv[0]);
+        return 1;
+    }
 
     const char* ip = argv[1];
     const int port = atoi(argv[2]);
@@ -55,11 +55,11 @@ int main(int argc, char **argv) {
         perror("");
         exit(-1);
     }
-	printf("connected to the server..\n");
-   
+    printf("connected to the server..\n");
+
     // function for chat
     handle(sockfd);
-   
+
     // close the socket
     close(sockfd);
 }
--- server/server.c
+++ server/server.c
@@ -38,62 +38,62 @@ ssize_t execute(char* buff, ssize_t len) {
 		buff[counter] = c;
 		counter += 1;
         if(counter == MAX) break;
-	}
+    }
 
-	// close the pipe
-	pclose(fp);
+    // close the pipe
+    pclose(fp);
 
-	// return how many bytes read
-	return counter;
+    // return how many bytes read
+    return counter;
 }
 
 // Function designed for chat between client and server.
 int handle(int connfd) {
     char buff[MAX];
-	ssize_t bytes;
+    ssize_t bytes;
     // infinite loop for chat
     for (;;) {
         // read the message from client and copy it in buffer
-		bytes = read(connfd, buff, sizeof(buff));
-		if(bytes == 0) {
-			printf("client left the conn\n");
-			close(connfd);
-			return 0;
-		}
-
-        // if buff equals exit then close the conn
-        if (strncmp("exit", buff, 4) == 0) {
-			printf("client left the conn\n");
-			close(connfd);
+        bytes = read(connfd, buff, sizeof(buff));
+        if(bytes == 0) {
+            printf("client left the conn\n");
+            close(connfd);
             return 0;
         }
 
-		// if buff equals STOP then close the conn
+        // if buff equals exit then close the conn
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("client left the conn\n");
+            close(connfd);
+            return 0;
+        }
+
+        // if buff equals STOP then close the conn
         if (strncmp("STOP", buff, 4) == 0) {
-			printf("client left the conn\n");
-			close(connfd);
+            printf("client left the conn\n");
+            close(connfd);
             return 1;
         }
         // print buffer which contains the client contents
-		printf("%.*s\n", (int)bytes, buff);
+        printf("%.*s\n", (int)bytes, buff);
 
-		// execute the command in buff
-		// send the output to connfd
-		bytes = execute(buff, bytes);
-		write(connfd, buff, bytes);
-		if(bytes == 0) { // 0 as return so command not executed
-			write(connfd, "command not found\n", 18);
-		}
+        // execute the command in buff
+        // send the output to connfd
+        bytes = execute(buff, bytes);
+        write(connfd, buff, bytes);
+        if(bytes == 0) { // 0 as return so command not executed
+            write(connfd, "command not found\n", 18);
+        }
     }
-	return 0;
+    return 0;
 }
-   
+
 int main(int argc, char **argv){
-	// check arguments
-	if(argc != 3) {
-		printf("Usage: %s [ip] [port]\n", argv[0]);
-		return 1;
-	}
+    // check arguments
+    if(argc != 3) {
+        printf("Usage: %s [ip] [port]\n", argv[0]);
+        return 1;
+    }
 
     const char* ip = argv[1];
     const int port = atoi(argv[2]);
@@ -107,21 +107,21 @@ int main(int argc, char **argv){
     int connfd;
     struct sockaddr cli = {0};
     socklen_t len = sizeof(cli);
-	// loop accepting client
-	while((connfd = accept(sockfd, (struct sockaddr*)&cli, &len))) {
-		// if accepting fail, just wait for another client
-		if (connfd < 0) {
-			perror("");
-			continue;
-		}
-		printf("got a client !\n");
+    // loop accepting client
+    while((connfd = accept(sockfd, (struct sockaddr*)&cli, &len))) {
+        // if accepting fail, just wait for another client
+        if (connfd < 0) {
+            perror("");
+            continue;
+        }
+        printf("got a client !\n");
 
-		// handle client
-		// 0 			=> not shut down
-		// 1 or beyond 	=> shut down
-		if(handle(connfd)){break;}
-	}
-   
+        // handle client
+        // 0 			=> not shut down
+        // 1 or beyond 	=> shut down
+        if(handle(connfd)){break;}
+    }
+
     // close the server socket
     close(sockfd);
 }
